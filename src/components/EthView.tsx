317ef3b9e3b7db22566daa43f70ae3dad68873a1
dankelleher <dankelleher@yahoo.com>
2022-10-13 15:36:44 +0200

    Add Eth Connector and dummy sign UI

--- src/components/EthView.tsx
+++ src/components/EthView.tsx
@@ -1,13 +1,18 @@
-import {Badge, Box, Button, Center, Stack, useColorModeValue, VStack,} from '@chakra-ui/react';
-import {FC, useCallback, useEffect, useState} from "react";
+import {Box, Button, Center, Stack, Textarea } from '@chakra-ui/react';
+import {FC, useCallback, useState} from "react";
 import {useDID} from '../hooks/useDID';
 import {EthereumConnector} from "./EthConnector";
 import {useWeb3React} from "@web3-react/core";
-import {PublicKey} from "@solana/web3.js";
 
 export const EthView:FC = () => {
     const { document } = useDID();
     const { library, account } = useWeb3React();
+    const [ message, setMessage ] = useState<string>();
+    const [ signature, setSignature ] = useState<string>();
+
+    const sign = useCallback(() => {
+        library?.getSigner().signMessage(message).then(setSignature)
+    }, [library, message]);
 
     return (
         <>
@@ -17,6 +22,7 @@ export const EthView:FC = () => {
                         <EthereumConnector />
                     </Stack>
                     {account &&
+                        <Stack>
                         <Box
                             mt='1'
                             fontWeight='semibold'
@@ -26,6 +32,10 @@ export const EthView:FC = () => {
                         >
                             {account}
                         </Box>
+                            <Textarea value={message} placeholder="Message to sign" onChange={(e) => setMessage(e.target.value)} />
+                            <Textarea value={signature} placeholder="Signature"/>
+                            <Button onClick={sign}>Sign</Button>
+                        </Stack>
                     }
                 </Stack>
             </Center>
